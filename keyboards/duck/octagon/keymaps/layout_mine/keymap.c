e029d914765c4b1bff98e2f824846a5e73aece09
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2018-10-30 22:21:22 -0500

    moved to 3 1.12u left of spacebar

--- keyboards/duck/octagon/keymaps/layout_mine/keymap.c
+++ keyboards/duck/octagon/keymaps/layout_mine/keymap.c
@@ -13,7 +13,8 @@ enum {
 
 enum custom_keycodes {
     M_COPY = SAFE_RANGE,
-    M_PASTE
+    M_PASTE,
+    M_SEL_ALL
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -24,6 +25,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             case M_PASTE:
                 SEND_STRING(SS_LCTRL("v")); // selects all and copies
+                return false;
+            case M_SEL_ALL:
+                SEND_STRING(SS_LCTRL("ac"));
                 return false;
         }
     }
@@ -110,13 +114,12 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
     [_BL] = LAYOUT_75_ansi(\
-        KC_LSPO,              KC_Z,                 KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,                      KC_MUTE,
-        TD(TD_ESC_CAPS),      KC_F1,                KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_PASTE, RGB_MOD,
-        KC_GRV,               KC_1,                 KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_QUOT,           RGB_TOG,
-        KC_TAB,               KC_Q,                 KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, TD(TD_ENTER),      KC_VOLU,
-        MO(_FL),              KC_A,                 KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,          KC_BSLS,           KC_VOLD,
-        KC_LSPO,              KC_Z,                 KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,                      KC_MUTE,
-        TD(TD_LCTL),          TD(TD_LALT), KC_NO,                KC_SPC,                                     KC_LALT, KC_NO,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
+        TD(TD_ESC_CAPS),      KC_F1,                KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_Q, RGB_MOD,
+        KC_GRV,               KC_1,                 KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,         KC_QUOT,    RGB_TOG,
+        KC_TAB,               KC_Q,                 KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   TD(TD_ENTER),    KC_VOLU,
+        MO(_FL),              KC_A,                 KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,                 KC_BSLS,    KC_VOLD,
+        KC_LSPO,              KC_Z,                 KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,                     KC_UP,  KC_MUTE,
+        TD(TD_LCTL),          KC_LGUI, TD(TD_LALT),                KC_SPC,                                               KC_LALT, KC_RGUI,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
 
     [_FL] = LAYOUT_75_ansi(\
         KC_TRNS, RGB_TOG, RGB_MOD, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
