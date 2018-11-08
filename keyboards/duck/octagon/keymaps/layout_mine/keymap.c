44733186c56b7c921388ec8a2c31beb9aee58510
Adhithya Bhaskar <bhaskar7@siebl-0220-01.ews.illinois.edu>
2018-11-08 17:10:20 -0600

    shortcut layer stuff

--- keyboards/duck/octagon/keymaps/layout_mine/keymap.c
+++ keyboards/duck/octagon/keymaps/layout_mine/keymap.c
@@ -5,11 +5,16 @@
 #define _FL 1
 #define _SL 2
 
-#define TD_ESC TD(TD_ESC_CAPS)
-#define KC_OTAB LCTL(KC_T)
-#define KC_ROTAB LCTL(LSFT(KC_T))
-#define KC_CLOSE LCTL(KC_X)
+#define S_ESC TD(TD_ESC_CAPS)
+#define S_OTAB LCTL(KC_T)
+#define S_ROTAB LCTL(LSFT(KC_T))
+#define S_CLOSE LCTL(KC_X)
 #define KC_DWORD LCTL(KC_BSPC)
+
+#define S_UP LCTL(KC_HOME)
+#define S_DOWN LCTL(KC_END)
+#define S_LEFT LCTL(KC_LEFT)
+#define S_RIGHT LCTL(KC_RIGHT)
 
 //Window management
 #define WIN_SQ1 (LCTL(LGUI(LALT(KC_1))))
@@ -58,7 +63,11 @@ enum custom_keycodes {
     M_COPY = SAFE_RANGE,
     M_PASTE,
     M_SALL,
-    M_UENT
+    M_UENT,
+    M_UP,
+    M_DOWN,
+    M_LEFT,
+    M_RIGHT
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -128,18 +137,26 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BL] = LAYOUT_75_ansi(\
-        TD_ESC,   WIN_SQ1,  WIN_SQ2,  WIN_SQ3,  WIN_SQ4,  WIN_L,  WIN_D,  WIN_U,  WIN_R,   WIN_M,   WIN_C,   KC_F11,  KC_F12,  KC_NLCK, BL_TOGG, RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_QUOT,    RGB_TOG,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,     KC_MUTE,
-        MO(_FL),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,            KC_ENTER,    KC_VOLU,
-        KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,                 KC_UP,  KC_VOLD,
-        KC_LCTL,  KC_LGUI, KC_LALT,                       KC_SPC,                            TD(TD_CP), KC_RGUI,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+        S_ESC,      WIN_SQ1,  WIN_SQ2,  WIN_SQ3,  WIN_SQ4,  WIN_L,  WIN_D,  WIN_U,  WIN_R,   WIN_M,   WIN_C,   KC_F11,  KC_F12,  KC_NLCK, BL_TOGG, RGB_MOD,
+        TD(TD_CP),   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_QUOT,       RGB_TOG,
+        KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,        KC_MUTE,
+        MO(_FL),     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,            KC_ENTER,       KC_VOLU,
+        KC_LSPO,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,                 KC_UP,     KC_VOLD,
+        KC_LCTL,     KC_LGUI, KC_LALT,                                KC_SPC,                            MO(_SL), KC_RGUI,   KC_RCTL, KC_LEFT, KC_DOWN,    KC_RGHT),
 
     [_FL] = LAYOUT_75_ansi(\
-        KC_TRNS, KC_F1,     KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12, KC_TRNS, KC_TRNS, RESET,
-        KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_ASTG,
-        KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_OTAB, KC_ROTAB, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_ASUP,
-        KC_TRNS, M(M_SALL), KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_DWORD,          KC_TRNS,         KC_ASDN,
-        KC_TRNS, KC_TRNS,   KC_CLOSE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,                   KC_ASRP,
-        KC_TRNS, KC_TRNS,   KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS),
+        KC_TILD, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12, KC_TRNS, KC_TRNS, RESET,
+        KC_GRV,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_ASTG,
+        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_ASUP,
+        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_DWORD,          KC_TRNS,        KC_ASDN,
+        KC_TRNS, KC_TRNS, S_CLOSE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,                  KC_ASRP,
+        KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS),
+
+    [_SL] = LAYOUT_75_ansi(\
+        KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, S_OTAB, S_ROTAB, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, M(M_SALL), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  S_LEFT,  S_DOWN,  S_UP,    S_RIGHT, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,   KC_TRNS,          KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
