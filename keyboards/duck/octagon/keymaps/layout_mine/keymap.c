ddfe6c323d209fffccb0df763435868081e99d49
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2018-11-07 22:34:17 -0600

    window map

--- keyboards/duck/octagon/keymaps/layout_mine/keymap.c
+++ keyboards/duck/octagon/keymaps/layout_mine/keymap.c
@@ -112,10 +112,13 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_CP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, cp_finished, cp_reset)
 };
 
+void indicator_leds_set(bool leds[8]);
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
     [_BL] = LAYOUT_75_ansi(\
-        TD_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_Q, RGB_MOD,
+        TD_ESC,   WIN_SQ1,  WIN_SQ2,  WIN_SQ3,  WIN_SQ4,  WIN_L,  WIN_D,  WIN_U,  WIN_R,   WIN_M,   WIN_C,  KC_F11,  KC_F12,  KC_PSCR, KC_Q, RGB_MOD,
         KC_GRV,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_QUOT,    RGB_TOG,
         KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,     KC_MUTE,
         MO(_FL),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,            KC_ENTER,    KC_VOLU,
@@ -123,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LGUI, KC_LALT,    KC_SPC,                                       TD(TD_CP), KC_RGUI,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
 
     [_FL] = LAYOUT_75_ansi(\
-        KC_TRNS, RGB_TOG,   RGB_MOD,  RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
+        KC_TRNS, KC_F1,     KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12, KC_TRNS, KC_TRNS, RESET,
         KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_ASTG,
         KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_OTAB, KC_ROTAB, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_ASUP,
         KC_TRNS, M(M_SALL), KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_DWORD,          KC_TRNS,         KC_ASDN,
