660eb8d3511f00e88166571ae18c7a854dbe754b
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2018-11-07 23:04:06 -0600

    testing indicator leds

--- keyboards/duck/octagon/keymaps/layout_mine/keymap.c
+++ keyboards/duck/octagon/keymaps/layout_mine/keymap.c
@@ -22,6 +22,19 @@
 #define WIN_M (LCTL(LGUI(LALT(KC_M))))
 #define WIN_C (LCTL(LGUI(LALT(KC_C))))
 
+//testing stuff
+
+void backlight_init_ports(void);
+// void indicator_leds_set([1 1 1 1 1 1 1 1]);
+// void indicator_leds_set(bool leds[8]);
+// void backlight_toggle_rgb(bool enabled);
+// void backlight_set_rgb(uint8_t cfg[17][3]);
+// void backlight_init_ports(void);
+// void send_color(uint8_t r, uint8_t g, uint8_t b, enum Device device);
+// void show(void);
+
+//end testing stuff
+
 //Tap Dance
 enum {
     TD_ESC_CAPS=0,
