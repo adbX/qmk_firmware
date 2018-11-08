c853cd3bd3e024a66ddcff034462e91b63495053
Adhithya Bhaskar <bhaskar7@siebl-0220-01.ews.illinois.edu>
2018-11-08 16:42:42 -0600

    formatting

--- keyboards/duck/octagon/keymaps/layout_mine/keymap.c
+++ keyboards/duck/octagon/keymaps/layout_mine/keymap.c
@@ -55,7 +55,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch(keycode) {
             case M_COPY:
-                SEND_STRING(SS_LCTRL("ac")); // selects all and copies               
+                SEND_STRING(SS_LCTRL("ac")); // selects all and copies
                 return false;
             case M_PASTE:
                 SEND_STRING(SS_LCTRL("v")); // selects all and copies
@@ -78,14 +78,14 @@ void cp_finished (qk_tap_dance_state_t *state, void *user_data) {
     if (state->interrupted || !state->pressed)
     {
         register_code(KC_LCTRL);
-        register_code(KC_C);    
+        register_code(KC_C);
     }
   }
   if (state->count == 2) {
     if (state->interrupted || !state->pressed)
     {
         register_code(KC_LCTRL);
-        register_code(KC_V);    
+        register_code(KC_V);
     }
   }
 }
@@ -95,14 +95,14 @@ void cp_reset (qk_tap_dance_state_t *state, void *user_data) {
     if (state->interrupted || !state->pressed)
     {
         unregister_code(KC_LCTRL);
-        unregister_code(KC_C);    
+        unregister_code(KC_C);
     }
   }
   if (state->count == 2) {
     if (state->interrupted || !state->pressed)
     {
         unregister_code(KC_LCTRL);
-        unregister_code(KC_V);    
+        unregister_code(KC_V);
     }
   }
 }
@@ -111,12 +111,12 @@ void cp_reset (qk_tap_dance_state_t *state, void *user_data) {
 //Enter previous command
 void up_enter_finished (qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) { register_code(KC_ENTER); }
-  else if (state->count == 2) { register_code(KC_UP); register_code(KC_ENTER); } 
+  else if (state->count == 2) { register_code(KC_UP); register_code(KC_ENTER); }
 }
 
 void up_enter_reset (qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) { unregister_code(KC_ENTER); }
-  else if (state->count == 2) { unregister_code(KC_UP); unregister_code(KC_ENTER); } 
+  else if (state->count == 2) { unregister_code(KC_UP); unregister_code(KC_ENTER); }
 }
 
 //Tap Dance Definitions
@@ -129,14 +129,14 @@ void indicator_leds_set(bool leds[8]);
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	
+
     [_BL] = LAYOUT_75_ansi(\
-        TD_ESC,   WIN_SQ1,  WIN_SQ2,  WIN_SQ3,  WIN_SQ4,  WIN_L,  WIN_D,  WIN_U,  WIN_R,   WIN_M,   WIN_C,  KC_F11,  KC_F12,  KC_PSCR, KC_Q, RGB_MOD,
-        KC_GRV,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_QUOT,    RGB_TOG,
-        KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,     KC_MUTE,
-        MO(_FL),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,            KC_ENTER,    KC_VOLU,
-        KC_LSPO,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,                 KC_UP,  KC_VOLD,
-        KC_LCTL,  KC_LGUI, KC_LALT,    KC_SPC,                                       TD(TD_CP), KC_RGUI,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
+        TD_ESC,   WIN_SQ1,  WIN_SQ2,  WIN_SQ3,  WIN_SQ4,  WIN_L,  WIN_D,  WIN_U,  WIN_R,   WIN_M,   WIN_C,   KC_F11,  KC_F12,  KC_PSCR, KC_Q, RGB_MOD,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_QUOT,    RGB_TOG,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,     KC_MUTE,
+        MO(_FL),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_BSPC,            KC_ENTER,    KC_VOLU,
+        KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,                 KC_UP,  KC_VOLD,
+        KC_LCTL,  KC_LGUI, KC_LALT,                       KC_SPC,                            TD(TD_CP), KC_RGUI,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FL] = LAYOUT_75_ansi(\
         KC_TRNS, KC_F1,     KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12, KC_TRNS, KC_TRNS, RESET,
