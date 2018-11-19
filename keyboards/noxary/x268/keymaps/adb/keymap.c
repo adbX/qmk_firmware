690e4c756b493dab64021e23e7ef361bbcf4f67a
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2018-11-19 02:15:35 -0600

    lz main!

--- keyboards/noxary/x268/keymaps/adb/keymap.c
+++ keyboards/noxary/x268/keymaps/adb/keymap.c
@@ -56,10 +56,10 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BL] = LAYOUT_65_ansi(
         TD_ESC,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_QUOT, KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC,          KC_BSLS, KC_MUTE,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC,          KC_QUOT, KC_MUTE,
         MO(FL),  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_BSPC, KC_NUHS,          KC_ENT,  KC_VOLU,
-        MO(SL),  KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_VOLD,
-        KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                    MO(WL),  MO(FL),   KC_NUHS, KC_LEFT,          KC_DOWN, KC_RGHT),
+        KC_LSFT, MO(SL),  KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_VOLD,
+        KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                    MO(WL),  KC_LGUI,  KC_NUHS, KC_LEFT,          KC_DOWN, KC_RGHT),
 
     [FL] = LAYOUT_65_ansi(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  _______, BL_TOGG,  RGB_TOG,
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                   _______,  _______, _______, _______,          RGB_M_SW, RGB_VAD),
 
     [SL] = LAYOUT_65_ansi(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______, _______, _______, _______, _______, S_LEFT,  S_DOWN,  S_UP,    S_RIGHT, _______, S_DWORD, _______,          _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
