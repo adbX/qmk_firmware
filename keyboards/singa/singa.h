28ba1878b85c5b1dcf91d461ad7e71cbf44607ac
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2018-12-07 18:55:58 -0600

    working key layout for split left shift

--- keyboards/singa/singa.h
+++ keyboards/singa/singa.h
@@ -84,8 +84,8 @@
   { k16,   k17,   k18,   k19,   k20,   k21,   k22,   k23,   k24,   k25,   k26,   k27,   k28,   k29, k81 }, \
   { k31,   k32,   k33,   k34,   k35,   k36,   k37,   k38,   k39,   k40,   k41,   k42,   k43,   k44, k45 }, \
   { k46,   k47,   k48,   k49,   k50,   k51,   k52,   k53,   k54,   k55,   k56,   k57,   KC_NO, k58, k59 }, \
-  { k60,   KC_NO, k61,   k62,   k63,   k64,   k65,   k66,   k67,   k68,   k69,   k70,   k71,   k72, k73 }, \
-  { k74,   KC_NO, k82,   k75,   KC_NO, k76,   KC_NO, KC_NO, k77,   KC_NO, KC_NO, KC_NO, k78,   k79, k80 }, \
+  { k60,   k82,   k61,   k62,   k63,   k64,   k65,   k66,   k67,   k68,   k69,   k70,   k71,   k72, k73 }, \
+  { k74,   KC_NO, k75,   KC_NO, KC_NO, k76,   KC_NO, KC_NO, k77,   KC_NO, KC_NO, KC_NO, k78,   k79, k80 }, \
   { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, k15, k30 } \
 }
 
