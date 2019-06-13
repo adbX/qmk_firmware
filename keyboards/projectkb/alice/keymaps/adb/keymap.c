eaa091bc4775c27ded2b6e64942a5a4e59b8375e
adb <adhithyabhaskar@gmail.com>
2019-06-13 14:16:20 -0500

    game

--- keyboards/projectkb/alice/keymaps/adb/keymap.c
+++ keyboards/projectkb/alice/keymaps/adb/keymap.c
@@ -4,8 +4,9 @@
 extern keymap_config_t keymap_config;
 
 #define BL 0
-#define SL 1
-#define FL 2
+#define GAME 1
+#define SL 2
+#define FL 3
 
 #define TD_ESC TD(TD_ESC_CAPS)
 #define S_OTAB LCTL(KC_T)
@@ -45,8 +46,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  TD_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSLS,
     KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_QUOT, KC_RBRC,
     KC_PGDN, MO(SL),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSPC,          KC_ENT,
-             KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             TG(SL), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, MO(FL),
+             KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             TG(GAME), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, MO(FL),
              KC_LCTL,          KC_LALT, MO(SL),  KC_RGUI,                          KC_SPC,           KC_RALT,          KC_RCTL
+  ),
+
+  [GAME] = LAYOUT_default(
+    _______,  _______, _______, _______, _______, _______, _______,   _______,   _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_VOLU,  _______, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_VOLD,  KC_LCTL, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______, _______,          _______,
+              _______, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______, _______, _______, _______,
+              _______,          _______, KC_SPC, _______,                            _______,          _______,          _______
   ),
 
   [SL] = LAYOUT_default(
