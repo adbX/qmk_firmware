3fdcc502dea8cb47d5345f428c3d4e5b8f70d74a
adb <adhithyabhaskar@gmail.com>
2019-09-03 12:34:04 -0500

    update

--- keyboards/hhkb/keymaps/adb/keymap.c
+++ keyboards/hhkb/keymaps/adb/keymap.c
@@ -1,32 +1,40 @@
 #include QMK_KEYBOARD_H
 
 #define BASE 0
-#define MAC 1
-#define FL 2
-#define SL 3
+#define GAME 1
+#define MAC 2
+#define FL 3
+#define SL 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, TG(MAC),
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
         KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_QUOT,      KC_RBRC,
         MO(FL),      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_BSPC,         KC_ENT,
-        KC_LSPO,       KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_VOLU,
-                    KC_LCTL, KC_LALT,                    KC_SPC,           KC_LGUI, KC_VOLD),
+        KC_LSPO,       KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, TG(GAME),
+                    KC_LCTL, KC_LALT,                    KC_SPC,           KC_LGUI, TG(MAC)),
+
+    [GAME] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,
+        KC_LCTL,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______,
+        KC_LSFT,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                    _______, KC_LALT,                    _______,           KC_LALT, _______),
 
     [MAC] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,
         _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______,
-        KC_LSFT,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_LSPO,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                     _______, KC_LGUI,                    _______,           KC_LALT, _______),
 
     [FL] = LAYOUT(
         RESET,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_INS, KC_GRV,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK,  KC_PAUS, KC_UP,   KC_BTN1, KC_BSPC,
         _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, KC_DEL, KC_PENT,
-        _______, _______, _______, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN,  KC_DOWN, _______, _______,
-        _______, _______, _______, _______, _______),
+        _______, _______, _______, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN,  KC_DOWN, _______, KC_VOLU,
+        _______, _______, _______, _______, KC_VOLD),
 
     [SL] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_GRV,
--- keyboards/projectkb/alice/keymaps/adb/rules.mk
+++ keyboards/projectkb/alice/keymaps/adb/rules.mk
@@ -1,1 +1,2 @@
 TAP_DANCE_ENABLE = yes
+DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
